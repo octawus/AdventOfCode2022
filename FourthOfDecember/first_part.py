a2559ca2788b42de044f502cca941cc223437321
Octawus <mihai_stanescu700@yahoo.es>
2022-12-04 17:00:42 +0100

    Solved the fourth of december

---
+++ FourthOfDecember/first_part.py
@@ -0,0 +1,32 @@
+import re
+
+f = open("cleaning_instructions", "r")
+#f = open("small_personal_example", "r")
+
+lines = f.readlines()
+
+no_of_containements = 0
+
+def asert_if_contained(first_elf_set, second_elf_set):
+    
+    if first_elf_set.issubset(second_elf_set) or second_elf_set.issubset(first_elf_set):
+        return True
+    else:
+        return False  
+
+#print(asert_if_contained(set(range(1,4)), set(range(4,8))))
+for line in lines:
+    
+    elf_asignements = re.split(r'[,-]', line.rstrip())
+    
+    first_elf_set_of_sections = set(range(int(elf_asignements[0]), int(elf_asignements[1])+1))
+    second_elf_set_of_sections = set(range(int(elf_asignements[2]), int(elf_asignements[3])+1))
+    
+    if asert_if_contained(first_elf_set_of_sections, second_elf_set_of_sections):
+        print(f"In this asignement there is containement {elf_asignements}")
+        no_of_containements = no_of_containements + 1
+    else:
+        print(f"There is no containement {elf_asignements}")
+
+
+print(no_of_containements)
